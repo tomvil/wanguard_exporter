5054c746adf96cfb4254caaa0f97556591fd2cb2
Tomas Vilemaitis <tomas.vilemaitis@gmail.com>
2024-10-28 14:29:36 +0200

    Add componentsCollector tests

---
+++ collectors/components_collecor_test.go
@@ -0,0 +1,37 @@
+package collectors
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus/testutil"
+	wgc "github.com/tomvil/wanguard_exporter/client"
+)
+
+func TestComponentsCollector(t *testing.T) {
+	wgcClient := wgc.NewClient(os.Getenv("TEST_SERVER_URL"), "u", "p")
+	ComponentsCollector := NewComponentsCollector(wgcClient)
+
+	metricsCount := testutil.CollectAndCount(ComponentsCollector)
+	if metricsCount != 3 {
+		t.Errorf("Expected 3 metric, got %d", metricsCount)
+	}
+
+	expectedMetrics := componentsExpectedMetrics()
+	err := testutil.CollectAndCompare(ComponentsCollector, strings.NewReader(expectedMetrics),
+		"wanguard_component_status")
+	if err != nil {
+		t.Errorf("Expected no error, got %s", err)
+	}
+}
+
+func componentsExpectedMetrics() string {
+	return `
+	# HELP wanguard_component_status Status of the component
+	# TYPE wanguard_component_status gauge
+	wanguard_component_status{component_category="bgp_connector",component_name="BGP Connector 1"} 1
+	wanguard_component_status{component_category="filter",component_name="Packet Filter 1"} 1
+	wanguard_component_status{component_category="sensor",component_name="Flow Sensor 1"} 1
+`
+}
